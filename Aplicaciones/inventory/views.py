71e6e1cf714bfadad2ee57aed6b7363e613aae1e
Sergio Parra <stevenparracuesta@gmail.com>
2026-03-29 12:17:36 -0500

    Sealiza vistas para inventory

--- Aplicaciones/inventory/views.py
+++ Aplicaciones/inventory/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+def home(request):
+    dataProductos = models.Producto.objects.all()
+    dataTotal = models.Producto.objects.count
+    data = {
+        'Productos': dataProductos,
+        'Total': dataTotal
+    }
+    return render(request, 'inventory-home.html', data)
+
+def detalle(request, id_producto):
+    dataProducto = models.Producto.objects.get(pk = id_producto)
+    data = { 'producto' : dataProducto }
+    return render(request, 'inventory-detail.html', data)
